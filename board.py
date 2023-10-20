3dd7a2eb84e1c75ada3969f3c1647bc8316e728f
BK <betamemo@BK.local>
2023-10-20 12:25:41 +0700

    go left

--- board.py
+++ board.py
@@ -16,12 +16,12 @@ class Board():
         self.screen = screen
         self.tiles = [[]]
         self.tiles = [
-            [0, 2, 2, 0],
+            [0, 2, 0, 8],
             [0, 0, 0, 0],
-            [0, 4, 0, 0],
+            [0, 0, 0, 4],
             [0, 0, 0, 0]
         ]
-        self.void_pos = (3, 3)
+        self.new_tiles = []
         self.draw_tiles()
 
     def draw_tiles(self):
@@ -34,12 +34,21 @@ class Board():
             self.turtle.goto(STARTING_X, self.turtle.ycor() - SPACE)
 
     def update_tiles(self):
+        # print(self.tiles)
+        # print(self.new_tiles)
+
+        tmp = self.tiles
+        self.tiles = self.new_tiles
+        self.new_tiles = tmp
+
         self.draw_tiles()
         self.screen.update()
 
     def go_left(self):
-        for row in range(len(self.tiles) - 1):
-            for col in range(len(self.tiles[row]) - 1):
-                if self.tiles[row][col] == 0:
-                    self.tiles[row][col], self.tiles[row][col + 1] = self.tiles[row][col + 1], self.tiles[row][col]
-                    self.update_tiles()
+        for row in self.tiles:
+            # move
+            num = [i for i in row if i > 0]
+            zero = [i for i in row if i == 0]
+            self.new_tiles.append(num + zero)
+
+        self.update_tiles()
