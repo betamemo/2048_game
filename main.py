afdcdee0476d1c8650ab1eb838fee286f9bc548f
BK <betamemo@BK.local>
2023-10-21 21:30:11 +0700

    use cmd to control

--- main.py
+++ main.py
@@ -19,3 +19,34 @@ class Board:
 
         self.add_tiles(board)
         return board
+
+
+board = Board(4, 4)
+print(board)
+
+game_is_on = True
+while game_is_on:
+    key = input('wasd?: ')
+
+    if key == 'w':  # move up
+        board.transpose()
+        board.move('left')
+        board.transpose()
+        board.add_tiles()
+        print(board)
+    elif key == 'a':  # move left
+        board.move('left')
+        print(board)
+    elif key == 's':  # move down
+        board.transpose()
+        board.move('right')
+        board.transpose()
+        print(board)
+    elif key == 'd':  # move right
+        board.move('right')
+        print(board)
+    elif key == 'q':  # quit
+        print('quit')
+        game_is_on = False
+    else:
+        print('invalid key!')
