992e2fc4456446b6eb10f68b686399396ef7cb2a
BK <betamemo@BK.local>
2023-10-22 21:34:49 +0700

    go left and right

--- board.py
+++ board.py
@@ -8,64 +8,66 @@ SPACE = 80
 
 class Board():
 
-    def __init__(self, screen):
+    def __init__(self, screen, row=4, col=4):
         self.turtle = Turtle()
         self.turtle.penup()
         self.turtle.hideturtle()
         self.turtle.goto(STARTING_X, STARTING_Y)
         self.screen = screen
-        self.tiles = [
+        self.board = [
             [0, 2, 2, 2],
             [0, 0, 0, 0],
             [0, 0, 0, 4],
             [0, 0, 0, 0]
         ]
+        self.row = row
+        self.col = col
+        self.new_board = []
         self.draw_tiles()
 
     def draw_tiles(self):
         self.turtle.clear()
         self.turtle.goto(STARTING_X, STARTING_Y)
-        for row in self.tiles:
+        for row in self.board:
             for col in row:
                 self.turtle.write(f'{col}', align='center', font=FONT)
                 self.turtle.goto(self.turtle.xcor() + SPACE, self.turtle.ycor())
             self.turtle.goto(STARTING_X, self.turtle.ycor() - SPACE)
 
-    def update_tiles(self):
-        # test
-        print(self.tiles)
+    def update_board(self):
+        tmp = self.board
+        self.board = self.new_board
+        self.new_board = tmp
 
         self.draw_tiles()
         self.screen.update()
 
-    def get_num(self, l):
-        return [i for i in l if i > 0]
+    def move(self, direction):
+        self.new_board = []
+        for row in self.board:
+            while 0 in row:
+                row.remove(0)
 
-    def get_zero(self, l):
-        return [i for i in l if i == 0]
+                # merge
+                for i in range(len(row) - 1):
+                    if row[i] == row[i + 1]:
+                        row[i] *= 2
+                        row[i + 1] = 0
 
-    def move_left(self):
-        for i in range(len(self.tiles)):
-            self.tiles[i] = self.get_num(self.tiles[i]) + self.get_zero(self.tiles[i])
-
-    def move_right(self):
-        for i in range(len(self.tiles)):
-            self.tiles[i] = self.get_zero(self.tiles[i]) + self.get_num(self.tiles[i])
+            if direction == 'Left':
+                while len(row) < self.row:
+                    row.append(0)
+            elif direction == 'Right':
+                while len(row) < self.row:
+                    row.insert(0, 0)
+            self.new_board.append(row)
+        self.update_board()
 
     def go_left(self):
+        self.move('Left')
 
-        # move
-        self.move_left()
-
-        # merge
-        # for x in range(len(self.tiles)):
-        #     for y in range(len(self.new_tiles[x]) - 1):
-        #         if self.new_tiles[x][y] > 0 and self.new_tiles[x][y] == self.new_tiles[x][y + 1]:
-        #             self.new_tiles[x][y] += self.new_tiles[x][y + 1]
-        #             self.new_tiles[x][y + 1] = 0
-
-        # display
-        self.update_tiles()
+    def go_right(self):
+        self.move('Right')
 
 
 screen = Screen()
@@ -78,7 +80,7 @@ screen.listen()
 # screen.onkeypress(fun=board.go_up, key='Up')
 # screen.onkeypress(fun=board.go_down, key='Down')
 screen.onkeypress(fun=board.go_left, key='Left')
-# screen.onkeypress(fun=board.go_right, key='Right')
+screen.onkeypress(fun=board.go_right, key='Right')
 
 screen.update()
 
